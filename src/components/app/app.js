f7e671fa98838a8ad57f8478d22382a6ff240a56
Shaktyka <k-i-s-u-n-j-a@list.ru>
2021-11-20 17:50:37 +0500

    Добавляет компоненты поиска

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,10 +1,17 @@
 import './app.css';
 import AppInfo from '../app-info/app-info';
+import SearchPanel from '../search-panel/search-panel';
+import AppFilter from '../app-filter/app-filter';
 
 const App = () => {
   return (
     <div className="app">
       <AppInfo />
+
+      <div className="search-panel">
+        <SearchPanel />
+        <AppFilter />
+      </div>
     </div>
   );
 }
